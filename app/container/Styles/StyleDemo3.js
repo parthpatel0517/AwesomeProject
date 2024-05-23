ea0ff96a4dfcbfdeffe7ec632115ddf290b05452
parthpatel0517 <parthpatel0517@gmail.com>
2024-05-23 19:48:58 +0530

    24_5_24_styales

--- app/container/Styles/StyleDemo3.js
+++ app/container/Styles/StyleDemo3.js
@@ -1,21 +1,56 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet,Image } from 'react-native'
 import React from 'react'
+
 
 export default function StyleDemo3() {
   return (
-    <View style={[styles.container,{
-        flexDirection: 'column',
-    }
-    ]}>
-     <View style={{flex: 1, backgroundColor: 'darkorange'}} ></View>
-     <View style={{flex : 2, backgroundColor: 'green'}} ></View>
-    </View>
-    
+
+    <View style={style.container}>
+      <View style={style.box1}></View>
+      <View style={style.box2}></View>
+      <View style={style.box3}></View>
+    <Image
+      source={{uri:'https://picsum.photos/id/237/200/300 '}}
+      style={{width:100,height:100}}
+    />
+    <Image
+      source={require('../../assets/Img/9b.jpg')} 
+      style={{width:100,height:100}}
+    />
+
+    </View >
   )
 }
-const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            padding: 20,
-          }
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    position: 'relative',
+    columnGap: 10
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    top: 50,
+    right: 50,
+    backgroundColor: 'blue'
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    top: 50,
+    backgroundColor: 'green'
+  },
+  box3: {
+    width: 100,
+    height: 100,
+    top: 50,
+    backgroundColor: 'red'
+  }
 })
+
+
+
+
